7b447414a9584fe37090cb1cf1b6598446397a5e
Kelly Ng <kellyng@Kellys-MacBook-Pro.local>
2016-02-14 13:08:36 -0700

    Refactor code into objects (as much as possible).  Minimum basic functionality of Frogger game complete.

--- js/app.js
+++ js/app.js
@@ -1,56 +1,66 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function(y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = x;
+
+    // Setting the Enemy initial location
     this.y = y;
+    this.x = 0;
+
+	// Setting the Enemy speed
     this.speed = speed;
-    // Setting the Enemy initial location (you need to implement)
-	// Setting the Enemy speed (you need to implement)
 
 	// Collision Detection - Bounding Box
 	this.width = 50;
 	this.height = 50;
+
 };
 
-// Update the enemy's position, required method for game
+function randomSpeed() {
+	return Math.random()*500;
+};
+
+function randomRow() {
+
+	var rowArray = [226, 143, 60]
+    return rowArray[Math.floor(Math.random()*(3))];
+
+};
+
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 
-    // Updates the Enemy location (you need to implement)
-	// Handles collision with the Player (you need to implement)
+    // Updates the Enemy location
 	this.x = this.x + this.speed*dt;
 	if (this.x > 505) {
 		this.x = 0;
 	};
 
+	// Handles collision
 	player.checkCollision();
-
-
 
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-var Player = function(x, y) {
+// Player class
+var Player = function() {
 
 	// Use superclass Enemy to construct Player
-	Enemy.call(this, x, y);
+	Enemy.call(this);
 	this.sprite = 'images/char-boy.png';
+
+	// Set initial position
+	this.x = 200;
+	this.y = 383;
 
 };
 
@@ -84,12 +94,14 @@ Player.prototype.reset = function() {
 		setTimeout(function(){player.setpos()}, delay);
 	}
 	if (this.checkCollision() == true) {
-		this.setpos();
+		this.resetpos();
 	};
 
 };
 
-Player.prototype.setpos = function() {
+Player.prototype.resetpos = function() {
+
+	// Reset player back to origin
 	this.x = 200;
 	this.y = 383;
 };
@@ -99,7 +111,7 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function(key) {
-	// redo with canvas.width and canvas.height
+
 	if (key == 'left' && this.x > 0) {
 		this.x -= 101;
 	};
@@ -114,17 +126,13 @@ Player.prototype.handleInput = function(key) {
 	};
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+// Instantiate 4 enemies and player
 var allEnemies = [];
-first = new Enemy(0,60,500);
-second = new Enemy(0,143,200);
-third = new Enemy(0,226,400);
-allEnemies.push(first, second, third);
+for (i = 0; i < 4; i++) {
+	allEnemies[i] = new Enemy(randomRow(), randomSpeed());
+};
 
-var player = new Player(100,300);
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,17 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Gem_Blue.png',
+        'images/Gem_Green.png',
+        'images/Gem_Orange.png',
+        'images/Heart.png',
+        'images/Key.png',
+        'images/Selector.png'
     ]);
     Resources.onReady(init);
 
